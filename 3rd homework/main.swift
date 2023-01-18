1b64e57dc8f8fa0c0d971f913397a2314b514abb
atayqq <ataykylychbekov96@gmail.com>
2023-01-18 17:57:36 +0600

    homework

--- 3rd homework/main.swift
+++ 3rd homework/main.swift
@@ -3,15 +3,47 @@
 func simbols(text: String){
     var totalString = ""
     var count = 0
-    
+
     for _ in text{
         count += 1
-        
+
     }
     totalString += "В строке - \(count) символов"
     print (totalString)
 }
-simbols(text: "как дела")
+print("Введите слово")
+let readline = readLine()!
+
+simbols(text: readline)
 
 //Задание 2
 
+func abc(text: String) {
+    var char = ""
+    var numChur = 0
+
+    for o in text {
+        if o != " " && o != "," && o != "." {
+            numChur += 1
+        }
+    }
+        char = "В тексте \(numChur) букв"
+        print(char)
+    }
+   print ("Введите текст")
+   let readline1 = readLine()!
+
+abc(text: readline1)
+
+//Задание 3
+
+
+var tripleA = readLine()!
+var triple3 = ""
+func AAA() {
+    for _ in tripleA{
+        triple3 += "A"
+    }
+    print("\(triple3.count)")
+}
+AAA()
